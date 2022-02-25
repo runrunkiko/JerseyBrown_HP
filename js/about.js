0e724f2985e44d7fc502674cd84b4a55d6469289
runrunkiko <kiko.enomoto.0620@gmail.com>
2022-02-25 18:36:49 +0900

    areaNav_OK

--- js/about.js
+++ js/about.js
@@ -4,3 +4,13 @@ $(function(){
     $("html, body").animate({scrollTop: 0 }, 600, "swing");
   });
 });
+// ページ内遷移
+$(function(){
+    $('#areaNav a[href*="#"]').click(function(){
+        var elmHash = $(this).attr('href');
+        var pos = $(elmHash).offset().top;
+        $('body,html').animate({scrollTop: pos}, 500);
+        return false;
+        console.log('きたよ');
+    });
+});
